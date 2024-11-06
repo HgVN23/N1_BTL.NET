2fe68971942eaa8e97562c5282d3e28634dde735
Shuuya <enderhoang@gmail.com>
2024-11-06 15:01:41 +0700

    Form main & db

---
+++ DAO/MenuInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_Nhom_1.DAO
+{
+    internal class MenuInfo
+    {
+        public int ID;
+        public string MenuItemName;
+        public string CategoryName;
+        public decimal? Price;
+        public int CategoryId;
+
+        public override string ToString()
+        {
+            return ID + "---" + MenuItemName + "---" + CategoryName + "---" + Price + "----" + CategoryId;
+        }
+    }
+}
---
+++ DAO/TableInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_Nhom_1.DAO
+{
+    internal class TableInfo
+    {
+        public int ID { get; set; }
+        public string TableName { get; set; }
+        public decimal? Price { get; set; }
+        public bool Status { get; set; }
+        public int FloorId { get; set; }
+    }
+}
